3d5ac41dd5d4ababfbb4054b500e834ecb5ac81b
Sasha Muzyka <sacha.muzika@gmail.com>
2025-04-25 17:25:17 +0300

    Add project files.

---
+++ SP_HW_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+
+//1
+//Thread countdownThread = new Thread(Method);
+//countdownThread.Start();
+
+//Thread.Sleep(4000); 
+
+//Stopwatch stopwatch = new Stopwatch();
+//stopwatch.Start();
+
+//Console.WriteLine("Press any key");
+//Console.ReadKey();
+
+//stopwatch.Stop();
+
+//Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+
+//void Method() 
+//{
+//    Thread.Sleep(1000);
+//    Console.WriteLine("3");
+//    Thread.Sleep(1000);
+//    Console.WriteLine("2");
+//    Thread.Sleep(1000);
+//    Console.WriteLine("1");
+//    Thread.Sleep(1000);
+//    Console.WriteLine("Go...");
+//}
+
+//2
+
+Console.Write("Enter path: ");
+string filePath = Console.ReadLine();
+
+Thread fileReaderThread = new Thread(() => Method2(filePath));
+fileReaderThread.Start();
+
+Console.WriteLine("Main Thread");
+
+
+void Method2(string path)
+{
+    Console.WriteLine("Main Thread");
+    try
+    {
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (StreamReader reader = new StreamReader(fs, Encoding.UTF8))
+        {
+            while (!reader.EndOfStream)
+            {
+                char ch = (char)reader.Read();
+                Console.Write(ch);
+                Thread.Sleep(200);
+            }
+        }
+        Console.WriteLine("\nENd read file");
+    }
+    catch (FileNotFoundException)
+    {
+        Console.WriteLine("File not found");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+
